74d9652230ff67385678a922070eee248db21b8c
Asif Rahman <daem0n@asifrahman.org>
2010-12-15 14:47:40 -0500

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* 
+ * Author: FAU MLAB
+ */
+
+var frameDelay = 2000;
+var updateTimer;
+
+$(document).ready(function() {
+	// Maximize window
+	$(window).width(screen.width);
+	$(window).height(screen.height);
+	
+	// Dynamic resizing
+	resizeElements();
+	
+	// Start the updater
+	updateTimer = setTimeout(updateFrames,frameDelay);
+});
+
+var resizeElements = function() {
+	$('#frames_container').width($(window).width());
+	$('#frame1').width($('#frames_container').width()/2);
+	$('#frame2').width($('#frame1').width());
+	
+	$('#frame1_img').width($('#frame1').width());
+	$('#frame2_img').width($('#frame2').width());
+}
+$(window).resize(resizeElements);
+
+var updateFrames = function() {
+	$('#frame1_img').attr('src', $('#frame1_img').attr('src').substr( 0, $('#frame1_img').attr('src').indexOf('?') ) +'?'+Math.random());
+	$('#frame2_img').attr('src', $('#frame2_img').attr('src').substr( 0, $('#frame2_img').attr('src').indexOf('?') ) +'?'+Math.random());
+	updateTimer = setTimeout(updateFrames,frameDelay);
+};

fdd180f43522ef413785f532870010477c4c2428
Asif <arahman2@fau.edu>
2010-12-15 22:18:22 -0500

    - Updated the infinite loop timer (fixed) - Changed the updateFrames function to destroy and recreate the embed html code for the embedded tiff to actually update

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	resizeElements();
 	
 	// Start the updater
-	updateTimer = setTimeout(updateFrames,frameDelay);
+	updateFrames();
 });
 
 var resizeElements = function() {
@@ -23,15 +23,29 @@ var resizeElements = function() {
 	$('#frame2').width($('#frame1').width());
 	$('#frame1').height($('#frame1').width() * 2050 / 2448);
 	$('#frame2').height($('#frame2').width() * 2050 / 2448);
+	resizeFrames();
+}
+$(window).resize(resizeElements);
+
+var resizeFrames = function() {
 	
 	$('#frame1_img').width($('#frame1').width());
 	$('#frame1_img').height($('#frame1_img').width() * 2050 / 2448);
 	$('#frame2_img').width($('#frame2').width());
 	$('#frame2_img').height($('#frame2_img').width() * 2050 / 2448);
+	
 }
-$(window).resize(resizeElements);
 
 var updateFrames = function() {
-	$('#frame1_img').attr('src', $('#frame1_img').attr('src').substr( 0, $('#frame1_img').attr('src').indexOf('?') ) +'?'+Math.random());
-	$('#frame2_img').attr('src', $('#frame2_img').attr('src').substr( 0, $('#frame2_img').attr('src').indexOf('?') ) +'?'+Math.random());
+	
+	$('#frame1').html("<embed id=\"frame1_img\" type=\"image/tiff\" src=\"cam2.tiff?"+Math.random()+"\" />");
+	$('#frame2').html("<embed id=\"frame2_img\" type=\"image/tiff\" src=\"cam1.tiff?"+Math.random()+"\" />");
+	
+	resizeFrames();
+	
+	//$('#frame1_img').attr('src', $('#frame1_img').attr('src').substr( 0, $('#frame1_img').attr('src').indexOf('?') ) +'?'+Math.random());
+	//$('#frame2_img').attr('src', $('#frame2_img').attr('src').substr( 0, $('#frame2_img').attr('src').indexOf('?') ) +'?'+Math.random());
+
+	updateTimer = setTimeout("updateFrames()",frameDelay);
+	
 };

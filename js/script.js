705631885179d45061000cfe7bcc8e92b3c20826
Asif <arahman2@fau.edu>
2010-12-15 21:51:17 -0500

    - Added tiff support (uses embed instead of img) - Changed frame delay to 5 seconds

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  * Author: FAU MLAB
  */
 
-var frameDelay = 2000;
+var frameDelay = 5000;
 var updateTimer;
 
 $(document).ready(function() {
@@ -21,9 +21,13 @@ var resizeElements = function() {
 	$('#frames_container').width($(window).width());
 	$('#frame1').width($('#frames_container').width()/2);
 	$('#frame2').width($('#frame1').width());
+	$('#frame1').height($('#frame1').width() * 2050 / 2448);
+	$('#frame2').height($('#frame2').width() * 2050 / 2448);
 	
 	$('#frame1_img').width($('#frame1').width());
+	$('#frame1_img').height($('#frame1_img').width() * 2050 / 2448);
 	$('#frame2_img').width($('#frame2').width());
+	$('#frame2_img').height($('#frame2_img').width() * 2050 / 2448);
 }
 $(window).resize(resizeElements);
 
